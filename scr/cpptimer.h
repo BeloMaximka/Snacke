dd4d82d85a6bae289c6dd65f83718461ec1e9b51
BeloMaximka <belomaxim@gmail.com>
2020-12-03 11:59:00 +0200

    реализовал таймер

--- scr/cpptimer.h
+++ scr/cpptimer.h
@@ -1,1 +1,24 @@
 #pragma once
+#include <chrono>
+
+typedef std::chrono::time_point<std::chrono::steady_clock> timer_time_point;
+
+struct timer
+{
+	timer_time_point TimeStart;
+	double DurationMiliSeconds;
+};
+
+void StartTimer(timer* Timer) {
+	Timer->TimeStart = std::chrono::steady_clock::now();
+}
+bool CheckTimerEnd(timer* Timer) {
+	if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - Timer->TimeStart).count() > Timer->DurationMiliSeconds)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
